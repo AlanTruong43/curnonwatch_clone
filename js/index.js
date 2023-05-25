0b59d91a075c47491a5ee390f7cdff4434113566
AlanTruong43 <truongvinhhoa3002@gmail.com>
2023-05-25 20:12:55 +0700

    11221

--- js/index.js
+++ js/index.js
@@ -8,7 +8,38 @@ $(document).ready(function(){
         $(this).find("i").css("color", "")
     })
 
-    $(".collection li").click(function(){
-        $(this).addClass(".collection__active");
+    
+
+    $("#collection1").click(function(){
+        $("div.collection").css("color","black");
+        $("ul.collection li a").css("color","black");
+        $("ul.collection li").css("border-top","2px solid black")
+        $(this).css("border-top","2px solid white")
+        $("div.collection img").attr("src","img index/banner__collection1.webp");
+        $("div.collection span").text("#Curnonwatch #MORAINE");
+        $("div.collection h1").text("MORAINE COLLECTION");
+        $("div.collection p").text("Nét tối giản, nữ tính nhưng không kém phần năng động cho 'cô nàng mùa hè'");
+    })
+
+    $("#collection2").click(function(){
+        $("div.collection").css("color","white");
+        $("ul.collection li a").css("color","white");
+        $("ul.collection li").css("border-top","2px solid black")
+        $(this).css("border-top","2px solid white")
+        $("div.collection img").attr("src","img index/banner__collection2.webp");
+        $("div.collection span").text("#COLOSSEUM #CURNONWATCH");
+        $("div.collection h1").text("COLOSSEUM COLLECTION");
+        $("div.collection p").text("Đại diện cho sự mạnh mẽ và phiêu lưu");
+    })
+
+    $("#collection3").click(function(){
+        $("div.collection").css("color","black");
+        $("ul.collection li a").css("color","black");
+        $("ul.collection li").css("border-top","2px solid black")
+        $(this).css("border-top","2px solid white")
+        $("div.collection img").attr("src","img index/banner__collection3.webp");
+        $("div.collection span").text("#HAMILTON #CURNONWATCH");
+        $("div.collection h1").text("HAMILTON COLLECTION");
+        $("div.collection p").text("Dòng đồng hồ nữ mặt vòm thanh lịch, hiện đại");
     })
 })
